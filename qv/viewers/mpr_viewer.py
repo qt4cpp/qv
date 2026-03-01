3531b9ddc4fc3f8f0cf622cb648674eb366b3a78
qt4cpp <qt4cpp@gmail.com>
2026-03-01 20:02:19 +0900

    implement set_image_data and reslice update.

--- qv/viewers/mpr_viewer.py
+++ qv/viewers/mpr_viewer.py
@@ -40,6 +40,13 @@ PLANE_AXES: dict[MprPlane, tuple[float, float, float, float, float, float, float
 }
 
 
+PLANE_AXES_INDEX: deict[MprPlane, int] = {
+    MprPlane.AXIAL: 2,
+    MprPlane.CORONAL: 1,
+    MprPlane.SAGITTAL: 0,
+}
+
+
 class MprViewer(BaseViewer):
     """2D MPR viewer."""
 
@@ -91,9 +98,84 @@ class MprViewer(BaseViewer):
 
     def set_image_data(self, image_data: vtk.vtkImageData) -> None:
         """Set the image data."""
-        self._image_data = image_data
         if self._reslice is None:
             return
-        self._reslice.SetInputData(image_data)
-        self.update_view()
 
+        self._image_data = image_data
+        self._reslice.SetInputData(image_data)
+
+        extent = image_data.GetExtent()
+        axis = PLANE_AXES_INDEX[self._plane]
+        self._slice_min = int(extent[2 * axis])
+        self._slice_max = int(extent[2 * axis + 1])
+        self._slice_index = (self._slice_min + self._slice_max) // 2
+
+        self._setup_camera(self._plane)
+        self._update_reslice()
+        self.update_view()
+        self.dataLoaded.emit()
+
+    def _update_reslice(self) -> None:
+        """Update the reslice parameters."""
+        if self._reslice is None:
+            return
+
+        extent = self._image_data.GetExtent()
+        spacing = self._image_data.GetSpacing()
+        origin = self._image_data.GetOrigin()
+
+        center_idx =[
+            (extent[0] + extent[1]) / 2.0,
+            (extent[2] + extent[3]) / 2.0,
+            (extent[4] + extent[5]) / 2.0,
+        ]
+        world_origin = [
+            origin[0] + center_idx[0] * spacing[0],
+            origin[1] + center_idx[1] * spacing[1],
+            origin[2] + center_idx[2] * spacing[2],
+        ]
+
+        axis = PLANE_AXES_INDEX[self._plane]
+        world_origin[axis] = origin[axis] + self._slice_index * spacing[axis]
+
+        self._reslice.SetResliceAxesOrigin(world_origin[0], world_origin[1], world_origin[2])
+        self._reslice.Modified()
+
+    def _setup_camera(self, plane: MprPlane) -> None:
+        """Configure the camera for the given plane."""
+        if self._image_data is None:
+            return
+
+        bounds = self._image_actor.GetBounds()
+        if bounds is None:
+            return
+
+        center = (
+            0.5 * (bounds[0] + bounds[1]),
+            0.5 * (bounds[2] + bounds[3]),
+            0.5 * (bounds[4] + bounds[5]),
+        )
+        max_dim = max(
+            bounds[1] - bounds[0],
+            bounds[3] - bounds[2],
+            bounds[5] - bounds[4],
+            1.0
+        )
+        dist = max_dim * 2.0
+
+        camera = self.renderer.GetActiveCamera()
+        camera.SetParallelProjection(True)
+        camera.SetFocalPoint(*center)
+
+        if plane == MprPlane.AXIAL:
+            camera.SetPosition(center[0], center[1], center[2] + dist)
+            camera.SetViewUp(0.0, 1.0, 0.0)
+        elif plane == MprPlane.CORONAL:
+            camera.SetPosition(center[0], center[1] - dist, center[2])
+            camera.SetViewUp(0.0, 0.0, 1.0)
+        else: # SAGITTAL
+            camera.SetPosition(center[0] + dist, center[1], center[2])
+            camera.SetViewUp(0.0, 0.0, 1.0)
+
+        self.renderer.ResetCamera()
+        self.renderer.ResetCameraClippingRange()
