b8057402a39d22d1153e103fc47a3717c43ee23a
qt4cpp <qt4cpp@gmail.com>
2026-09-20 17:44:59 +0900

    __main__: Force setting QT_QPA_PLATFORM to xcb.

--- qv/__main__.py
+++ qv/__main__.py
@@ -1,3 +1,10 @@
+import os
+
+# VTK(QVTKRenderWindowInteractor) がWaylandネイティブに非対応のため、
+# 強制的にX11(XWayland)プラットフォームにする。
+# 明示的に QT_QP_PLATFORM を設定している場合はそちらを優先する。
+os.environ.setdefault("QT_QPA_PLATFORM", "xcb")
+
 from qv.main import main
 
 if __name__ == "__main__":
